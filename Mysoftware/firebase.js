da4d37d1cd8da51b620793b2b31b18407ad441db
Vast Academy <148587341+Vast-Academy@users.noreply.github.com>
2023-11-27 10:42:52 +0530

    Add files via upload

---
+++ Mysoftware/firebase.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyB5-ow4YS3r98QIcfMyT4JLJJmzjDfmCbc",
+  authDomain: "authentication-608f0.firebaseapp.com",
+  projectId: "authentication-608f0",
+  storageBucket: "authentication-608f0.appspot.com",
+  messagingSenderId: "574744795723",
+  appId: "1:574744795723:web:aedc90dce5a85278a58bce",
+  measurementId: "G-EBFQEFYH2P"
+};
+
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
